7d9e7ddac421e021f0b8cc5ed31156a504dd7523
hidura <35303597+hidura@users.noreply.github.com>
2021-12-26 11:17:25 -0400

    Update Connect.py

--- tools/DataBase/Connect.py
+++ tools/DataBase/Connect.py
@@ -11,13 +11,13 @@ from mongoengine import connect
 
 class conection:
     def conORM(self):
-        return create_engine("postgresql+pypostgresql://sugelico:vgi3se1fq7k4fhox"
-                             "@web1-bmsuiteio-do-user-4013959-0.db.ondigitalocean.com:25060/sugelico?sslmode=require",poolclass=NullPool)
+        return create_engine("postgresql+pypostgresql://username:password"
+                             "@hostname:portname/dbname",poolclass=NullPool)
 
 
     def conODM(self):
 
-        return connect(db='sugelico_erp_test', alias='default', username='sugelico',
-                       password='#GbUsa1776@', port=29642, host='209.97.153.183')
+        return connect(db='dbname', alias='default', username='username',
+                       password='password', port=port, host='hostname')
 
 
