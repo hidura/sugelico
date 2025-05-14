97c6e2c0d01342dfe53942678fdf218d2ad100e3
Diego Hidalgo <35303597+hidura@users.noreply.github.com>
2025-05-14 15:23:37 -0400

    Update Payments.py

--- tools/Payments/Azul/Payments.py
+++ tools/Payments/Azul/Payments.py
@@ -41,10 +41,7 @@ class Azul:
         gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
         self.request = Request(url, data=urlencode(data).encode('utf-8'))
         self.request.add_header("CONTENT_TYPE", "multipart/form-data;charset=utf-8")
-        self.request.add_header("Auth1", "eps")
-        self.request.add_header("Auth2", "eps")
-        #self.request.add_header('X-Mashape-Key', '39036630010')
-
+        
         self.request.add_header('Accept', 'application/json')
         self.request.add_header("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:33.0) Gecko/20100101 Firefox/33.0")
         # check error
